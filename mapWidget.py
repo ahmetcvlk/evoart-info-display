ff2a077604ef83acf51b016637e6ee9080ae6d20
ahmetcvlk <cvlkahmet@hotmail.com>
2025-05-13 20:13:15 +0300

    temp change - before change the branch

--- mapWidget.py
+++ mapWidget.py
@@ -9,7 +9,7 @@ class MapWidget(QWidget):
         self.layout = QVBoxLayout(self)
         
         self.map_label = QLabel(self)
-        self.map_pixmap = QPixmap("Arayuz\map.png")  # Harita görselini yüklüyoruz
+        self.map_pixmap = QPixmap("traffic-signs\map.png")  # Harita görselini yüklüyoruz
         self.map_label.setPixmap(self.map_pixmap)
         self.layout.addWidget(self.map_label)
 
